1f235859bbdffc4bd09068c778f42de00c24a2e9
Lua Jeon <nyancat2323.1232@gmail.com>
2023-12-01 11:27:45 +0900

    add handler of parquet

---
+++ pages/parquet_handler.py
@@ -0,0 +1,88 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+from pyplus.streamlit import streamlit_plus as sp,streamlit_plus_utility as spu
+
+
+tab_name = ['new','open']
+file_tab = st.tabs(tab_name)
+with file_tab[0]:
+
+    new_df_dtype = pd.DataFrame(columns=['name','dtype','index'])
+    new_df_dtype['index'] = new_df_dtype['index'].astype("boolean")
+    #all_dtype=pd.read_html("https://pandas.pydata.org/docs/user_guide/basics.html#basics-dtypes")[3]["String Aliases"]
+    #all_dtype = all_dtype.apply(lambda s:s.split(",")).apply(lambda l:[s.strip("'`") for s in l])
+    #all_dtype
+    all_dtype=["datetime64[ns]"\
+                ,"category"\
+                #,"period[<freq>]"\
+                #,'Sparse', 'Sparse[int]', 'Sparse[float]'\
+                #,'interval', 'Interval[<numpy_dtype>]', 'Interval[datetime64[ns, <tz>]]','Interval[timedelta64[<freq>]]'\
+                ,'Int8', 'Int16', 'Int32', 'Int64', 'UInt8', 'UInt16', 'UInt32', 'UInt64'\
+                ,'Float32', 'Float64'\
+                ,'string'\
+                ,'boolean'
+                ]
+    new_df_dtype_column_config = {
+        "dtype": st.column_config.SelectboxColumn(
+            label="Set dtype",
+            options=all_dtype
+        )
+    }
+    new_df_dtype = st.data_editor(new_df_dtype,num_rows="dynamic",hide_index=True, column_config=new_df_dtype_column_config)
+    
+
+    new_df = pd.DataFrame({a:pd.Series([],dtype=b) for a,b in zip(new_df_dtype['name'],new_df_dtype['dtype'])})
+    new_df=st.data_editor(new_df,num_rows="dynamic")
+    try:
+        column_for_index = new_df_dtype[new_df_dtype['index']==True]['name'].to_list()
+        column_for_index
+        new_df = new_df.set_index(column_for_index)
+    except:
+        pass
+    st.dataframe(new_df)
+    filename = st.text_input(label='filename',value='out')
+    st.download_button(label='download parquet (new)',data=new_df.to_parquet(),file_name=f'{filename}.parquet')
+
+with file_tab[1]:
+    new_df=None
+    try:
+        file = st.file_uploader('parquet test',type='parquet')
+        file_df = pd.read_parquet(path=file,)
+    except:
+        pass
+    try:
+        file_df=st.data_editor(file_df,num_rows="dynamic")
+    except:
+        "No file"
+    st.markdown("---")
+
+
+    process = st.tabs(['drop','rename','set_index','dtype'])
+
+    with process[0]:
+        try:
+            input = sp.list_checkbox(*file_df.columns.values)
+            remove_select = [key for key,value in input.items() if value==True]
+            file_df = file_df.drop(remove_select,axis=1)
+        except:
+            pass
+
+    with process[1]:
+        try:
+            changer = sp.list_text_input_by_vals(*file_df.columns.values)
+            changer = {key : value for key,value in changer.items() if len(value)>0 }
+            changer
+            file_df = file_df.rename(mapper=changer,axis=1)
+        except:
+            pass
+    
+    with process[2]:
+        try:
+            index = st.text_input('setting index')
+            file_df = file_df.set_index([index])
+        except:
+            st.error("Index error")
+
+    st.dataframe(file_df)
+    st.download_button(label=f'download parquet',data=file_df.to_parquet(),file_name='out.parquet')
---
+++ pages/parquet_upload.py
@@ -0,0 +1,56 @@
+import streamlit as st
+import pandas as pd
+from pyplus.streamlit.streamlit_plus_utility import FileDescription,execute_file_descriptions
+from typing import List
+import re
+def read_from_server(table_name,conn,schema_name):
+    with conn.connect() as conn_conn:
+        return pd.read_sql_table(table_name=table_name,con=conn_conn,schema=schema_name)
+
+def get_direction(df_file:pd.DataFrame,df_to:pd.DataFrame):
+    st.subheader("move columns")
+    with conn.connect() as conn_conn:
+        ret_dict={}
+
+        for col_file in df_file.reset_index().columns:
+            if st.checkbox(f'move {col_file}',value=True):
+                ret_dict[col_file]=st.selectbox(f' move {col_file} to',df_to.columns)
+
+        return ret_dict
+
+conn = st.connection(name='postgresql',type='sql')
+with st.sidebar:
+    fds : List[FileDescription] = []
+    fds.append(FileDescription("^[A-Za-z0-9_]+.parquet$",pd.read_parquet))
+    dfs = execute_file_descriptions(fds)
+
+
+for key in dfs:
+    schema = st.text_input('Input of schema')
+    table = st.text_input("Input of table")
+    df_from = dfs[key]
+    df_to = read_from_server(table,conn,schema)
+
+    direction = get_direction(df_from,df_to)
+    not_in_directions = list(filter(lambda value:value not in direction.values(),df_to.columns))
+    default_values={}
+    for column in not_in_directions:
+        if st.checkbox(f'fill {column}',value=True):
+            default_values[column] = st.text_input(f'{column}')
+    with st.expander('direction'):
+        direction
+        default_values
+    if st.button(f'{key} upload to server'):
+        df_file_sql = dfs[key].copy()
+        df_file_sql = df_file_sql.rename(columns=direction)
+        try:
+            df_file_sql.index = df_file_sql.index.rename(direction[df_file_sql.index.name])
+            _indexed = True
+        except:
+            _indexed = False
+            st.write('no index in sql')
+        for key,val in default_values.items():
+            df_file_sql[key] = val
+        df_file_sql
+        with conn.connect() as conn_conn:
+          df_file_sql.to_sql(table,conn_conn,schema,if_exists='append',index=_indexed)
