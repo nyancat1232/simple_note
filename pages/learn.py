35411cebc462834cf95577032df563d5881e994e
Lua Jeon <nyancat2323.1232@gmail.com>
2023-12-01 11:27:46 +0900

    learning

--- pages/learn.py
+++ pages/learn.py
@@ -1,8 +1,8 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-from typing import List
 from pyplus.sql.pgplus import read_from_server
+import tensorflow as tf
 
 from pyplus.streamlit.external import check_password
 if not check_password():
@@ -10,4 +10,24 @@ if not check_password():
 
     
 conn=st.connection('postgresql',type='sql')
+
+v=read_from_server(st.secrets['study']['learn_schema'],st.secrets['study']['learn_table'],st_conn=conn)
+v
+
+
+xtr=np.array([ [x,x+1] for x in range(0,100,3)])
+ytr=np.array([ sum(x) for x in xtr])
+xtr
+ytr
+
+#model creation
+inputs = tf.keras.Input(shape=(len(xtr[0])))
+hidden = tf.keras.layers.Dense(units=100,activation='relu')
+outputs = hidden(inputs)
+model = tf.keras.Model(inputs=inputs,outputs=outputs)
+model.compile(loss=tf.keras.losses.mean_squared_error)
+
+history = model.fit(x=xtr,y=ytr)
+
+test = model.evaluate([1,2],[3])
 
