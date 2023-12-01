21dc4747aa8182cf03a622052858cbc10f4d23c7
Lua Jeon <nyancat2323.1232@gmail.com>
2023-12-01 11:27:46 +0900

    id descending

--- pages/sql_editor.py
+++ pages/sql_editor.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from pyplus.streamlit.external import check_password
 from sqlutil.sql_util import r_d_sql,table_selection
-from pyplus.sql.pgplus import expand_foreign_column
+from pyplus.sql.pgplus import expand_foreign_column,get_identity
 
 if not check_password():
     st.stop()  # Do not continue if check_password is not True.
@@ -18,7 +18,9 @@ with st.sidebar:
 
 st.subheader('total')
 result_expand = expand_foreign_column(schema_name=input.schema,table_name=input.table,st_conn=st_connff)
-st.dataframe(result_expand)
+id = get_identity(schema_name=input.schema,table_name=input.table,st_conn=st_connff)
+
+st.dataframe(result_expand.sort_values(id.to_list(),ascending=False))
     
 r_d_sql(input.schema,input.table,st_connff)
 
