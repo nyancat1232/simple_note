5efc1ec48e45c65e6861498e2bc99436876c2f70
Lua Jeon <nyancat2323.1232@gmail.com>
2023-12-01 11:27:45 +0900

    two modes

--- pages/sql_editor.py
+++ pages/sql_editor.py
@@ -10,22 +10,24 @@ table = st.text_input("Input of table")
 
 df_sql_read = read_from_server(schema,table,conn)
 st.dataframe(df_sql_read,hide_index=True)
-df_sql = df_sql_read.copy()
-df_sql = df_sql.drop(labels=df_sql.index,axis=0)
-fks = get_foreign_keys(schema,table,conn)
-fks
-for fki,fkr in fks.iterrows():
-    r = read_from_server(fkr['upper_schema'],fkr['upper_table'],conn)
-    r
+
+cols = st.tabs(['append','edit'])
+
+with cols[0]:
+    df_sql = df_sql_read.copy()
+    df_sql = df_sql.drop(labels=df_sql.index,axis=0)
+    fks = get_foreign_keys(schema,table,conn)
+    fks
+    for fki,fkr in fks.iterrows():
+        r = read_from_server(fkr['upper_schema'],fkr['upper_table'],conn)
+        r
 
 
-identity = get_identity(schema,table,conn)
-#identity
-#df_sql
-df_sql = df_sql.drop(columns=identity)
-result = st.data_editor(df_sql,num_rows="dynamic",hide_index=True)
-    
+    identity = get_identity(schema,table,conn)
+    df_sql = df_sql.drop(columns=identity)
+    result = st.data_editor(df_sql,num_rows="dynamic",hide_index=True)
+        
 
 
-if st.button('upload'):
-    result.to_sql(table,conn.connect(),schema=schema,index=False,if_exists='append')
+    if st.button('upload'):
+        result.to_sql(table,conn.connect(),schema=schema,index=False,if_exists='append')
