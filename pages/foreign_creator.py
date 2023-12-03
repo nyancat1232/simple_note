582202767e1f4abe7a48c5688660e9baf083b9cb
Lua Jeon <nyancat2323.1232@gmail.com>
2023-12-03 20:28:09 +0900

    experimental

--- pages/foreign_creator.py
+++ pages/foreign_creator.py
@@ -1,12 +1,22 @@
 import streamlit as st
 import pandas as pd
 from pyplus.streamlit.external import check_password
-from pyplus.sql.pgplus import read_from_server,get_columns,write_to_server,create_empty_with_id_with_column
+from pyplus.sql.pgplus import read_from_server,get_columns,write_to_server,create_empty_with_id_with_column,create_columns
 from sqlutil.sql_util import table_selection
 
 if not check_password():
     st.stop()  # Do not continue if check_password is not True.
 
+
+def select_foreign_column(df) -> dict:
+    df_data_columns = get_columns(schema_name=input.schema,table_name=input.table,st_conn=st_connff)
+    foreign_column = st.multiselect(label='create as foreign key of',options=df.columns)
+    st.write('rr')
+
+    df_new_foreign = df[foreign_column].drop_duplicates()
+    df_new_foreign
+    _dict_col=df_data_columns.loc[foreign_column].to_dict()['data_type']
+    return df_new_foreign, _dict_col
 
     
 st_connff = st.connection(name='postgresql',type='sql')
@@ -17,45 +27,23 @@ with st.sidebar:
 
 st.subheader('total')
 df_data = read_from_server(schema_name=input.schema,table_name=input.table,st_conn=st_connff)
-df_data_colums = get_columns(schema_name=input.schema,table_name=input.table,st_conn=st_connff)
 st.dataframe(df_data)
-
-foreign_column = st.multiselect(label='create as foreign key of',options=df_data.columns)
-
-st.write('rr')
-
-df_create_foreign = df_data[foreign_column].drop_duplicates()
-df_create_foreign
-_dict_col=df_data_colums.loc[foreign_column].to_dict()['data_type']
-_dict_col
-
-#df_create_foreign.insert(0,'_id',0)
-#for one,two in enumerate(df_create_foreign.iterrows()):
-#    df_create_foreign.at[two[0],'_id']=one
-#
-#df_create_foreign
-#_merge_for_left={
-#    'left':df_data,
-#    'left_on':foreign_column
-#}
-#_merge_for_right={
-#    'right':df_create_foreign,
-#    'right_on':foreign_column
-#}
-#first_place=df_data.columns.to_list().index(foreign_column[0])
-#first_place
-#df_original = pd.merge(**_merge_for_left,**_merge_for_right,how='inner')
-#df_original = df_original.drop(columns=foreign_column)
-#
-#col_order = df_original.columns
-#col_order = col_order.drop('_id')
-#col_order = col_order.insert(first_place,'_id')
-#
-#df_original[col_order]
-
-def create_as_foreign_key(df_input_create_foreign):
+df_foreign, foreign_columns = select_foreign_column(df_data)
+foreign_columns
+if st.button('write'):
     write_to_server(df_data,input.schema,input.table+'_backup',st_connff)
-    create_empty_with_id_with_column(_dict_col,input.schema,input.table+'_foreign',st_connff)
-    write_to_server(df_create_foreign,input.schema,input.table+'_foreign',st_connff)
 
-st.button('write',on_click=create_as_foreign_key,args=[df_create_foreign])
+    foreign_table_inf={
+        'schema_name':input.schema,
+        'table_name':input.table+'_foreign',
+        'st_conn':st_connff
+    }
+
+    create_empty_with_id_with_column(foreign_columns,**foreign_table_inf)
+    write_to_server(df_foreign,**foreign_table_inf)
+
+    df_result_foreign = read_from_server(**foreign_table_inf)
+    df_result_foreign
+
+    create_columns({input.table+'_foreign'+'_id':'bigint'},input.schema,input.table,
+                        st_conn=st_connff)
