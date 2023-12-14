52ff040b700cf32ea0038103eaf47c27483877a3
Lua Jeon <nyancat2323.1232@gmail.com>
2023-12-15 07:05:14 +0900

    add new func

--- main.py
+++ main.py
@@ -7,15 +7,28 @@ if not check_password():
     st.stop()  # Do not continue if check_password is not True.
 
 
+from datetime import datetime,timezone,timedelta
 sumhandler = SummaryHandler()
-sumhandler['max']=max
-def func_duration(sr):
-    from datetime import datetime,timezone
+sumhandler['max']=lambda df,arg1,arg2,arg3:max(df[arg1])
+def func_duration(df,arg1,arg2,arg3):
+    sr= df[arg1]
     recent=max(sr)
     return datetime.now(tz=timezone.utc)-recent
 sumhandler['duration']=func_duration
-sumhandler['min']=min
-
+sumhandler['min']=lambda df,arg1,arg2,arg3:min(df[arg1])
+def get_weighted_mean_day(df:pd.DataFrame,arg1,arg2,arg3):
+    #arg1 => date
+    #arg2 => column
+    #arg3 => limit
+    df['_date_from_now']=datetime.now(tz=timezone.utc)-df[arg1]
+    more_than_day1=df['_date_from_now']>timedelta(days=1)
+    df['_weight']=1
+    df['_weight'][more_than_day1]=df['_date_from_now'][more_than_day1].apply(lambda t:timedelta(days=1)/t)
+    df['_weight']=df[arg2]*df['_weight']
+    
+    df_within_days=df[df['_date_from_now']<timedelta(days=int(arg3))]
+    return sum(df_within_days['_weight'])/len(df_within_days)
+sumhandler['mean_day']=get_weighted_mean_day
 
 
 cur_sch=st.secrets['summary']['schema']
@@ -27,7 +40,10 @@ for summary_idx in summaries.index:
     cur_row = summaries.loc[summary_idx]
     cur_tab = read_from_server(cur_row['schema_name'],cur_row['table_name'],conn)
     
-    sr_column = cur_tab[cur_row['column_name']]
-    
-    res = sumhandler[cur_row['function']](sr_column)
-    res
+    try:
+        res = sumhandler[cur_row['function']](cur_tab,cur_row['function_arg1'],cur_row['function_arg2'],cur_row['function_arg3'])
+        st.subheader(f"{cur_row['schema_name']}.{cur_row['table_name']} {cur_row['function_arg1']} {cur_row['function_arg2']}")
+        st.write(res)
+    except:
+        st.write(f"{cur_row['function']} not working")
+    st.divider()
