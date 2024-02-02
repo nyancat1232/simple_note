d48842c40f359c9959f462db8dafc1a9c53a6715
Lua Jeon <nyancat2323.1232@gmail.com>
2024-02-02 14:53:55 +0900

    create table

---
+++ pages/create_table.py
@@ -0,0 +1,35 @@
+import streamlit as st
+from pyplus.streamlit.external import check_password
+from sqlutil.sql_util_new import table_selector
+import pyplus.sql as sqlp
+import pyplus.streamlit as stp
+
+if not check_password():
+    st.stop()  # Do not continue if check_password is not True.
+
+
+conn=st.connection('simple_note',type='sql')
+
+@stp.init_session('list_of_schema')
+def init_schema():
+    lists=sqlp.get_table_list(conn.engine)
+    return lists['table_schema'].unique().tolist()
+
+schema_list = init_schema(refresh=True)
+
+types=['bigint','double precision','text']
+
+schema_list
+schema_name = st.text_input('schema name')
+table_name = st.text_input('table name')
+
+
+cols = {'':None}
+sttype = {'value':st.column_config.SelectboxColumn('test',options=types)}
+result = st.data_editor(cols,num_rows='dynamic',column_config=sttype)
+result
+
+if st.button('create table'):
+    ts = sqlp.TableStructure(schema_name=schema_name,table_name=table_name,engine=conn.engine)
+    res = ts.create_table(**result)
+    res
