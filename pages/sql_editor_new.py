0f70655765f0144db2ecaf89a8fa82bcc39b5000
Lua Jeon <nyancat2323.1232@gmail.com>
2024-04-09 22:44:21 +0900

    refresh and move

--- pages/sql_editor_new.py
+++ pages/sql_editor_new.py
@@ -27,6 +27,21 @@ def get_custom_column_configs(ts:sqlp.TableStructure):
         column_configs[col] = st.column_config.ImageColumn(f'{col}',)
     return column_configs
 
+def df_empty_records(df:pd.DataFrame)->pd.DataFrame:
+    df_append = df.copy()
+    df_append = df_append.loc[0:0]
+    return df_append.reset_index(drop=True)
+
+def extract_foreign_column(ts:sqlp.TableStructure)->tuple[set,set]:
+    df_read = ts.read()
+    df_expanded = ts.read_expand(remove_original_id=True)
+    col_ex = set(df_expanded.columns.to_list())
+    col_r = set(df_read.columns.to_list())
+    col_non_foreign = col_ex&col_r
+    col_foreign_ex = col_ex-col_non_foreign
+    col_foreign_r = col_r-col_non_foreign
+    return col_foreign_r,col_foreign_ex
+
 custom_configs = get_custom_column_configs(first_ts)
 
 df_read = first_ts.read()
@@ -57,26 +72,14 @@ if st.button('upload'):
 
 st.subheader('append mode')
 
-def df_empty_records(df:pd.DataFrame)->pd.DataFrame:
-    df_append = df.copy()
-    df_append = df_append.loc[0:0]
-    return df_append.reset_index(drop=True)
 df_append = df_empty_records(df_expanded)
 
 for col in df_read.columns:
     df_append[col] = pd.Series([None for _ in df_append.index])
 
-def extract_foreign_column(ts:sqlp.TableStructure)->tuple[set,set]:
-    df_read = ts.read()
-    df_expanded = ts.read_expand()
-    col_ex = set(df_expanded.columns.to_list())
-    col_r = set(df_read.columns.to_list())
-    col_non_foreign = col_ex&col_r
-    col_foreign_ex = col_ex-col_non_foreign
-    col_foreign_r = col_r-col_non_foreign
-    return col_foreign_r,col_foreign_ex
 
 col_foreign,col_foreign_expanded = extract_foreign_column(first_ts)
+
 if len(col_foreign)>0:
     foreign_expand = st.multiselect('expand foreign column',col_foreign,col_foreign)
 
