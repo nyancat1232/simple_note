3afcb4eb5e9b78ae041a2acad41ff256f6d6ad39
Lua Jeon <nyancat2323.1232@gmail.com>
2024-04-10 17:51:09 +0900

    new method

--- pages/sql_editor_new.py
+++ pages/sql_editor_new.py
@@ -34,6 +34,34 @@ def get_custom_column_configs(ts:sqlp.TableStructure):
     
     return column_configs
 
+def select_yielder(gen:Generator,msg:str):
+    '''
+    catch a return of generator. yield type must be like (msg:str,value:Any).
+    
+    Parameters
+    ----------
+    gen : Generator
+        generator that yields (msg,value).
+    
+    Returns
+    --------
+    Any
+        a value of yield.
+    
+    Examples
+    --------
+    >>> def test_yield(first_msg):
+    >>>     yield f'{first_msg}: apple', 3
+    >>>     yield f'{first_msg}: banaba', 1
+    >>>     yield f'{first_msg}: cherry', 2
+    >>> 
+    >>> aa = select_yielder(test_yield('Rick'),'Rick: banaba')
+    >>> aa
+    1
+    '''
+    for current_msg,ret in gen:
+        if current_msg == msg:
+            return ret
 def extract_foreign_column(ts:sqlp.TableStructure)->tuple[set,set]:
     df_read = ts.read()
     df_expanded = ts.read_expand(remove_original_id=True)
