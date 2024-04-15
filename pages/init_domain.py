4c3783f39ed9dfbaf0e791697bb2c4cf0fef6259
Lua Jeon <nyancat2323.1232@gmail.com>
2024-04-15 16:34:33 +0900

    creating config for database

--- pages/init_domain.py
+++ pages/init_domain.py
@@ -9,3 +9,8 @@ ss = sqlp.SchemaStructure('public',conn.engine)
 if st.button('init domain'):
     ss.create_domain('url','text')
     ss.create_domain('image_url','text')
+
+if st.button('init setting'):
+    sn_conf = sqlp.create_schema(conn.engine,'sn_config')
+    ts = sn_conf.create_table('configs',**{'name':'text','val':'text'})
+    ts.upload_append(**{'name':'recent_address'})
