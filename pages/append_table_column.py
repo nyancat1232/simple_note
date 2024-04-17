a664cec0ac53c73d1f1a67232ee8bfab031388bd
Lua Jeon <nyancat2323.1232@gmail.com>
2024-04-17 17:03:01 +0900

    adding foreign column

--- pages/append_table_column.py
+++ pages/append_table_column.py
@@ -20,9 +20,19 @@ read_result = ts.read_expand()
 read_result
 
 df = pd.DataFrame([{'name':'','type':''}])
-sttype = {'type':st.column_config.SelectboxColumn('test',options=types)}
+sttype = {'type':st.column_config.SelectboxColumn('test',options=types+['_foreign'])}
 result = st.data_editor(df,num_rows='dynamic',column_config=sttype)
 result = {rec['name']:rec['type'] for rec in result.to_dict(orient='records')}
+for col_local in [col for col in result if result[col] == '_foreign']:
+    del result[col_local]
+    schema,table = table_selector(f'{col_local} to a table ')
+    ts = sqlp.TableStructure(schema,table,conn.engine)
+    df_foreign = ts.read()
+
+    f'{col_local} to a table'
+    df_foreign
+
+    result[col_local] = f'_foreign:{schema}.{table}'
 result
 
 if st.button('append columns'):
