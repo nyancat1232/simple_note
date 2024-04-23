a11a8585ecd06d17ef48eabc828e661b88b1ed2e
Lua Jeon <nyancat2323.1232@gmail.com>
2024-04-24 04:48:35 +0900

    commit test

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,7 @@ read SERVERURL
 CONTAINER_NAME=simple_note_container
 APP_NAME=simple_note
 
+
 docker --host tcp://$SERVERURL:2375 stop $CONTAINER_NAME
 docker --host tcp://$SERVERURL:2375 rm $CONTAINER_NAME
 docker --host tcp://$SERVERURL:2375 image rm $APP_NAME
