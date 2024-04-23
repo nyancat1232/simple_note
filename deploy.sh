12338e9d8036dc64f0f8861703aa62d9381caec9
Lua Jeon <nyancat2323.1232@gmail.com>
2024-04-24 04:35:29 +0900

    remove

--- deploy.sh
+++ deploy.sh
@@ -3,16 +3,8 @@ read SERVERURL
 CONTAINER_NAME=simple_note_container
 APP_NAME=simple_note
 
-#developing environment
-#pipreqs . --encoding=utf-8 --force
-
-mkdir .streamlit
-cp $HOME/.streamlit/secrets.toml .streamlit
-
 docker --host tcp://$SERVERURL:2375 stop $CONTAINER_NAME
 docker --host tcp://$SERVERURL:2375 rm $CONTAINER_NAME
 docker --host tcp://$SERVERURL:2375 image rm $APP_NAME
 docker --host tcp://$SERVERURL:2375 buildx build --tag $APP_NAME .
 docker --host tcp://$SERVERURL:2375 run --detach --publish 8044:8044 --restart always --name $CONTAINER_NAME $APP_NAME 
-
-rm -rf .streamlit
