ab96c374adf420420fe77ef5e7526669c9068652
Lua Jeon <nyancat2323.1232@gmail.com>
2024-04-26 20:50:29 +0900

    add tag table

--- pages/init_domain.py
+++ pages/init_domain.py
@@ -13,3 +13,4 @@ if st.button('init setting'):
     sn_conf = sqlp.create_schema(conn.engine,'sn_config')
     ts = sn_conf.create_table('configs',**{'name':'text','val':'text'})
     ts.upload_append(**{'name':'recent_address'})
+    ts.upload_append(**{'name':'address_tags'})
