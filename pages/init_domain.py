7aba6abc916bf2b6f8af9751bf656ad5ed2e011c
Lua Jeon <nyancat2323.1232@gmail.com>
2024-04-29 19:41:45 +0900

    don't write the same code twice

--- pages/init_domain.py
+++ pages/init_domain.py
@@ -11,7 +11,11 @@ if st.button('init domain'):
 
 if st.button('init setting'):
     sn_conf = sqlp.create_schema(conn.engine,sn_config_table['schema'])
-    ts = sn_conf.create_table(sn_config_table['table'],**{'name':'text','val':'text'})
+    ts = sn_conf.create_table(sn_config_table['table'],**{'name':'text','value':'text'})
     ts.upload_append(**{'name':'recent_address'})
-    ts.upload_append(**{'name':'address_tags'})
-    ts.upload_append(**{'name':'address_timer'})
+
+    def upload_table_content(table_name,**types):
+        sn_conf.create_table(table_name,**types)
+        ts.upload_append(**{'name':f'address_{table_name}','value':f'{sn_config_table['schema']}.{table_name}'})
+    upload_table_content('tag_content',**{'content':'text','tags':'text[]','date':'timestamp with time zone'})
+    upload_table_content('timer_content',**{'content':'text','type':'text','start_time':'timestamp with time zone'})
