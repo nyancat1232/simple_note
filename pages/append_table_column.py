aa0eb2093861d12f01ff67b33ce6a8f8e74c71fe
Lua Jeon <nyancat2323.1232@gmail.com>
2024-06-01 04:50:53 +0900

    separate

--- pages/append_table_column.py
+++ pages/append_table_column.py
@@ -9,9 +9,9 @@ import pyplus.sql as sqlp
 
 
 with st.sidebar:
-    ts = table_selector()
+    ts_first = table_selector()
 
-read_result = ts.read_expand()
+read_result = ts_first.read_expand()
 read_result
 
 df = pd.DataFrame([{'name':'','type':''}])
@@ -20,14 +20,14 @@ result = st.data_editor(df,num_rows='dynamic',column_config=sttype)
 result = {rec['name']:rec['type'] for rec in result.to_dict(orient='records')}
 for col_local in [col for col in result if result[col] == '_foreign']:
     del result[col_local]
-    ts = table_selector(f'{col_local} to a table ')
-    df_foreign = ts.read()
+    ts_foreign = table_selector(f'{col_local} to a table ')
+    df_foreign = ts_foreign.read()
 
     f'{col_local} to a table'
     df_foreign
 
-    result[col_local] = f'_foreign:{ts.schema_name}.{ts.table_name}'
+    result[col_local] = f'_foreign:{ts_foreign.schema_name}.{ts_foreign.table_name}'
 result
 
 if st.button('append columns'):
-    ts.append_column(**result)
+    ts_first.append_column(**result)
