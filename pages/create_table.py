62cd1fb34ff9f7affc1803cfa5ae3f303fd4db81
Lua Jeon <nyancat2323.1232@gmail.com>
2024-06-06 14:52:57 +0900

    support other manipulation

--- pages/create_table.py
+++ pages/create_table.py
@@ -5,17 +5,22 @@ ex()
 import pyplus.sql as sqlp
 import pyplus.streamlit as stp
 
+tp = stp.TabsPlus('tab','create a table','append a column')
 
-schema_name = init_schema()
-table_name = st.text_input('table name')
+with tp['create a table']:
+    schema_name = init_schema()
+    table_name = st.text_input('table name')
 
 
-cols = {'':None}
-sttype = {'value':st.column_config.SelectboxColumn('test',options=types)}
-result_type = st.data_editor(cols,num_rows='dynamic',column_config=sttype)
-result_type
-if st.button('create table'):
-    ss = sqlp.SchemaStructure(schema_name=schema_name,engine=conn.engine)
-    res = ss.create_table(table_name,**result_type)
-    st.toast('succeed')
-    #res.upload_append(**{key:"" for key in result_type})
+    cols = {'':None}
+    sttype = {'value':st.column_config.SelectboxColumn('test',options=types)}
+    result_type = st.data_editor(cols,num_rows='dynamic',column_config=sttype)
+    result_type
+    if st.button('create table'):
+        ss = sqlp.SchemaStructure(schema_name=schema_name,engine=conn.engine)
+        res = ss.create_table(table_name,**result_type)
+        st.toast('succeed')
+        #res.upload_append(**{key:"" for key in result_type})
+
+with tp['append a column']:
+    pass
