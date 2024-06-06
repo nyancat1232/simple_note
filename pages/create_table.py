02fd1c939c3f8332d9333692282f09fd27cd27af
Lua Jeon <nyancat2323.1232@gmail.com>
2024-06-06 14:55:37 +0900

    abstract

--- pages/create_table.py
+++ pages/create_table.py
@@ -1,6 +1,6 @@
 import streamlit as st
-from pre import ex,conn,init_schema,types
-ex()
+import pre as stglobal
+stglobal.ex()
 
 import pyplus.sql as sqlp
 import pyplus.streamlit as stp
@@ -8,16 +8,16 @@ import pyplus.streamlit as stp
 tp = stp.TabsPlus('tab','create a table','append a column')
 
 with tp['create a table']:
-    schema_name = init_schema()
+    schema_name = stglobal.init_schema()
     table_name = st.text_input('table name')
 
 
     cols = {'':None}
-    sttype = {'value':st.column_config.SelectboxColumn('test',options=types)}
+    sttype = {'value':st.column_config.SelectboxColumn('test',options=stglobal.types)}
     result_type = st.data_editor(cols,num_rows='dynamic',column_config=sttype)
     result_type
     if st.button('create table'):
-        ss = sqlp.SchemaStructure(schema_name=schema_name,engine=conn.engine)
+        ss = sqlp.SchemaStructure(schema_name=schema_name,engine=stglobal.conn.engine)
         res = ss.create_table(table_name,**result_type)
         st.toast('succeed')
         #res.upload_append(**{key:"" for key in result_type})
