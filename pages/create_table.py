343e66df0f7d9f702e5286303afb564581765f74
Lua Jeon <nyancat2323.1232@gmail.com>
2024-06-06 16:50:54 +0900

    change: The table will be used globally

--- pages/create_table.py
+++ pages/create_table.py
@@ -7,31 +7,16 @@ stglobal.ex()
 import pyplus.sql as sqlp
 import pyplus.streamlit as stp
 
-tp = stp.TabsPlus('tab','create a table','append a column','change column name','change a column order')
 
 def upload_button(func,label):
     if st.button(label):
         func()
         st.toast(f'Succeed: {label}')
 
-with tp['create a table']:
-    schema_name = stglobal.init_schema()
-    table_name = st.text_input('table name')
+if (ts_first := stglobal.table_selector()) is not None:
+    tp = stp.TabsPlus('tab','append a column','change column name','change a column order')
 
-
-    cols = {'':None}
-    sttype = {'value':st.column_config.SelectboxColumn('test',options=stglobal.types)}
-    result_type = st.data_editor(cols,num_rows='dynamic',column_config=sttype)
-    result_type
-
-    def upload_create():
-        ss = sqlp.SchemaStructure(schema_name=schema_name,engine=stglobal.conn.engine)
-        ss.create_table(table_name,**result_type)
-
-    upload_button(upload_create,'create table')
-
-with tp['append a column']:
-    if (ts_first := stglobal.table_selector()) is not None:
+    with tp['append a column']:
         read_result = ts_first.read_expand()
         read_result
 
@@ -45,8 +30,7 @@ with tp['append a column']:
             ts_first.append_column(**result)
             st.rerun()
 
-with tp['change column name']:
-    if (ts_first := stglobal.table_selector('select a table for changing column names')) is not None:
+    with tp['change column name']:
         df = ts_first.read()
         df
         df_change = pd.DataFrame({'before':df.columns,'after':df.columns})
@@ -62,8 +46,8 @@ with tp['change column name']:
         def change_names():
             ts_first.change_column_name(**arg)
         upload_button(change_names,'change column names')
-with tp['change a column order']:
-    if (ts_first := stglobal.table_selector('select a table for changing a order')) is not None:
+
+    with tp['change a column order']:
         read_result = ts_first.read_expand()
         read_result
 
@@ -76,3 +60,20 @@ with tp['change a column order']:
             columns_after = [df_order['name'][ind] for ind in df_order['order']]
             df_order_after = pd.DataFrame({'name':columns_after}) 
             df_order_after
+else:
+    tp = stp.TabsPlus('tab','create a table')
+    with tp['create a table']:
+        schema_name = stglobal.init_schema()
+        table_name = st.text_input('table name')
+
+
+        cols = {'':None}
+        sttype = {'value':st.column_config.SelectboxColumn('test',options=stglobal.types)}
+        result_type = st.data_editor(cols,num_rows='dynamic',column_config=sttype)
+        result_type
+
+        def upload_create():
+            ss = sqlp.SchemaStructure(schema_name=schema_name,engine=stglobal.conn.engine)
+            ss.create_table(table_name,**result_type)
+
+        upload_button(upload_create,'create table')
