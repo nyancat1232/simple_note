520bbed39df93d83287d4256fefbc7860e61b50d
Lua Jeon <luajeon506@gmail.com>
2024-06-30 07:41:52 +0900

    change: main page

--- main.py
+++ main.py
@@ -4,25 +4,4 @@ import pandas as pd
 from pre import ex,conn
 ex()
 
-
-from simple_note_funcs.summary import *
-
-
-
-cur_sch=st.secrets['summary']['schema']
-cur_tab=st.secrets['summary']['table']
-
-summaries=read_from_server(cur_sch,cur_tab,conn)
-summaries
-for summary_idx in summaries.index:
-    cur_row = summaries.loc[summary_idx]
-    cur_tab = read_from_server(cur_row['schema_name'],cur_row['table_name'],conn)
-    
-    try:
-        res = sumhandler[cur_row['function']](cur_tab,*cur_row['function_args'])
-        st.subheader(f"{cur_row['schema_name']}.{cur_row['table_name']} {cur_row['function_args']}")
-        res
-        st.slider(**res,disabled=True)
-    except:
-        st.write(f"{cur_row['function']} not working")
-    st.divider()
+"Nothing"
