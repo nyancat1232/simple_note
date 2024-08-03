75aa12677392a0bbbc9007247262c5331353a05e
Lua Jeon <luajeon506@gmail.com>
2024-08-04 03:33:09 +0900

    new: table to markdown list

--- table_editor/reader.py
+++ table_editor/reader.py
@@ -16,6 +16,15 @@ temp=second_ts.get_foreign_tables()
 for col in temp:
     bb=second_ts.check_selfref_table(temp[col])
 
+order_nums=st.slider('size',min_value=1,max_value=len(df_with_tag.columns))
+order_selections=[st.selectbox(f'{num} selection',df_with_tag.columns) for num in range(order_nums)]
+result_list=[]
+for row in df_with_tag.to_dict('records'):
+    for ind,col in enumerate(order_selections):
+        result_list.append('\t'*ind+f'- {row[col]}')
+result_str="\n".join(result_list)
+st.code(result_str)
+
 custom_configs_ro:dict = bp.select_yielder(stglobal.iter_custom_column_configs(second_ts),'readonly')
 event=st.dataframe(df_with_tag,column_config=custom_configs_ro,on_select='rerun',selection_mode='single-row')
 row = df_with_tag.iloc[event['selection']['rows']].to_dict('records')[0]
