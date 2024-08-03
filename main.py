f5ea452880bd4fc2615ba63c581b7eb63007fcda
Lua Jeon <luajeon506@gmail.com>
2024-08-04 02:59:32 +0900

    change: unmerge

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ if 'conn' not in st.session_state:
     st.session_state['conn'] = st.connection('myaddress',type='sql')
 st.session_state['types']=['bigint','double precision','text','timestamp with time zone','boolean','url','image_url','video_url','text_with_tag']
 
-pg = st.navigation({'main':[st.Page('new_page/empty.py',title='empty'),
+pg = st.navigation({'main':[st.Page('table_editor/reader.py',title='reader'),
                             st.Page('table_editor/editor.py',title='editor'),
                             st.Page('table_editor/create_table.py',title='table or column editor'),
                             st.Page('table_editor/remover_table.py',title='row or column deletion'),
--- table_editor/reader.py
+++ table_editor/reader.py
@@ -136,74 +136,23 @@ for col in temp:
     bb=second_ts.check_selfref_table(temp[col])
     bb
 
-st.subheader('edit mode')
-custom_configs_rw:dict = bp.select_yielder(iter_custom_column_configs(second_ts),'edit')
-with st.expander('column config'):
-    custom_configs_rw
-df_edited = st.data_editor(df_with_tag,disabled=second_ts.column_identity,column_config=custom_configs_rw)
+custom_configs_ro:dict = bp.select_yielder(iter_custom_column_configs(second_ts),'readonly')
+event=st.dataframe(df_with_tag,column_config=custom_configs_ro,on_select='rerun',selection_mode='single-row')
+row = df_with_tag.iloc[event['selection']['rows']].to_dict('records')[0]
+types = second_ts.get_types_expanded().to_dict(orient='index')
+for col in types:
+    try:
+        if row[col]:
+            pass
+    except:
+        st.toast(f'skipping column {col}')
+        continue
 
-idname_df_edited = df_edited.index.name
-def func_melt(df:pd.DataFrame):
-    df_reset = df.copy().reset_index()
-    return df_reset.melt(id_vars='id',value_name='_sn_value')
-df_edited_melt=func_melt(df_edited)
-df_with_tag_melt=func_melt(df_with_tag)
-with st.expander('debug'):
-    tp_debug = stp.TabsPlus(layout='column',titles=['before','after'])
-    with tp_debug['before']:
-        df_edited_melt
-    with tp_debug['after']:
-        df_with_tag_melt
-    df_compared = df_edited_melt.compare(df_with_tag_melt)
-    changed=df_compared.index.to_list()
-    df_temp = df_edited_melt.loc[changed]
-    df_temp
-    recs=dict()
-    for temp in df_temp.to_dict('records'):
-        if temp['id'] not in recs:
-            recs[temp['id']] = {}
-        recs[temp['id']][temp['variable']] = temp['_sn_value']
-    recs
-if st.button('upload'):
-    for row_id in recs:
-        st.toast(f'{row_id}:{recs[row_id]}')
-        second_ts.upload(id_row=row_id,**recs[row_id])
-    st.rerun()
-
-
-st.subheader('append mode')
-
-df_append = pdp.empty_records(second_ts.read())
-df_append = df_append.reset_index(drop=True)
-
-tss_foreign = second_ts.get_foreign_tables()
-
-tab_or_col=stp.TabsPlus(layout='column',titles=tss_foreign,hide_titles=False)
-for col_local_foreign in tss_foreign:
-    ts_sub = tss_foreign[col_local_foreign]
-    df_display=ts_sub.read_expand()
-    conf = bp.select_yielder(iter_custom_column_configs(ts_sub),'readonly')
-    
-    #display
-    with tab_or_col[col_local_foreign]:
-        st.dataframe(df_display,column_config=conf)
-
-for col in df_append.columns.to_list():
-    if col.startswith('_'):
-        del df_append[col]
-cond_satisfies_warning = len(df_append.columns)<2
-if cond_satisfies_warning:
-    st.warning('Problem when column is only one. ValueError: setting an array element with a sequence')
-    df_append['__hidden']=df_append.index
-df_append = st.data_editor(df_append,num_rows='dynamic',column_config=custom_configs_rw)
-if cond_satisfies_warning:
-    st.warning('Problem when column is only one. ValueError: setting an array element with a sequence')
-    del df_append['__hidden']
-
-appends = df_append.to_dict(orient='records')
-with st.expander('upload preview'):
-    appends
-if st.button('append'):
-    second_ts.upload_appends(*appends)
-    st.toast('append')
-    st.rerun()
+    if row[col] is not None:
+        match types[col]['display_type']:
+            case 'image_url':
+                st.markdown(f'#### {col}')
+                st.image(row[col])
+            case 'video_url':
+                st.markdown(f'#### {col}')
+                st.video(row[col])
