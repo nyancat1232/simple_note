51877baccd6c8f9f5547e4e595310f7bc2a4f89d
Lua Jeon <luajeon506@gmail.com>
2024-09-26 06:11:54 +0900

    change: loss prevention while writing

--- table_editor/append_rows.py
+++ table_editor/append_rows.py
@@ -11,66 +11,65 @@ second_ts = sqlp.TableStructure(schema_name=st.session_state['current_address'][
 
 with st.sidebar:
     df_with_tag = bp.CheckPointFunction(stglobal.iter_tag_process)(second_ts).filter_tag()
+with st.form('append form',clear_on_submit=True):
+    temp=second_ts.get_foreign_tables()
+    for col in temp:
+        bb=second_ts.check_selfref_table(temp[col])
 
-temp=second_ts.get_foreign_tables()
-for col in temp:
-    bb=second_ts.check_selfref_table(temp[col])
-
-custom_configs_rw_def:dict = bp.CheckPointFunction(stglobal.iter_custom_column_configs)(second_ts).edit()
-custom_configs_rw_edit=custom_configs_rw_def.copy()
-st.dataframe(df_with_tag,column_config=custom_configs_rw_edit)
+    custom_configs_rw_def:dict = bp.CheckPointFunction(stglobal.iter_custom_column_configs)(second_ts).edit()
+    custom_configs_rw_edit=custom_configs_rw_def.copy()
+    st.dataframe(df_with_tag,column_config=custom_configs_rw_edit)
 
 
-custom_configs_rw_def:dict = bp.CheckPointFunction(stglobal.iter_custom_column_configs)(second_ts).edit()
+    custom_configs_rw_def:dict = bp.CheckPointFunction(stglobal.iter_custom_column_configs)(second_ts).edit()
 
-st.subheader('append mode')
-custom_configs_rw_append=custom_configs_rw_def.copy()
+    st.subheader('append mode')
+    custom_configs_rw_append=custom_configs_rw_def.copy()
 
-df_append = pdp.empty_records(second_ts.read())
-df_append = df_append.reset_index(drop=True)
+    df_append = pdp.empty_records(second_ts.read())
+    df_append = df_append.reset_index(drop=True)
 
-tss_foreign = second_ts.get_foreign_tables()
+    tss_foreign = second_ts.get_foreign_tables()
 
-selected_col_convert=dict()
-"Select a column"
-tab_or_col=stp.TabsPlus(layout='column',titles=tss_foreign,hide_titles=False)
-for col_local_foreign in tss_foreign:
-    ts_sub = tss_foreign[col_local_foreign]
-    df_display=ts_sub.read_expand()
-    conf = bp.CheckPointFunction(stglobal.iter_custom_column_configs)(ts_sub).readonly()
-    
-    #display
-    with tab_or_col[col_local_foreign]:
-        selected_col_convert[col_local_foreign]=st.dataframe(df_display,column_config=conf,selection_mode='single-column',on_select='rerun',key=f'convert_of_{col_local_foreign}')['selection']['columns']
-        if len(selected_col_convert[col_local_foreign])>0:
-            selected_col_convert[col_local_foreign]=selected_col_convert[col_local_foreign][0]
-            selections=df_display[selected_col_convert[col_local_foreign]].unique().dropna().tolist()
-            custom_configs_rw_append[col_local_foreign+'__conversion']=st.column_config.SelectboxColumn(f'{col_local_foreign}(conversion from {selected_col_convert[col_local_foreign]})',options=selections)
-            df_append[col_local_foreign+'__conversion']=pd.Series()
-            del df_append[col_local_foreign]
+    selected_col_convert=dict()
+    "Select a column"
+    tab_or_col=stp.TabsPlus(layout='column',titles=tss_foreign,hide_titles=False)
+    for col_local_foreign in tss_foreign:
+        ts_sub = tss_foreign[col_local_foreign]
+        df_display=ts_sub.read_expand()
+        conf = bp.CheckPointFunction(stglobal.iter_custom_column_configs)(ts_sub).readonly()
+        
+        #display
+        with tab_or_col[col_local_foreign]:
+            selected_col_convert[col_local_foreign]=st.dataframe(df_display,column_config=conf,selection_mode='single-column',on_select='rerun',key=f'convert_of_{col_local_foreign}')['selection']['columns']
+            if len(selected_col_convert[col_local_foreign])>0:
+                selected_col_convert[col_local_foreign]=selected_col_convert[col_local_foreign][0]
+                selections=df_display[selected_col_convert[col_local_foreign]].unique().dropna().tolist()
+                custom_configs_rw_append[col_local_foreign+'__conversion']=st.column_config.SelectboxColumn(f'{col_local_foreign}(conversion from {selected_col_convert[col_local_foreign]})',options=selections)
+                df_append[col_local_foreign+'__conversion']=pd.Series()
+                del df_append[col_local_foreign]
 
-for col in df_append.columns.to_list():
-    if col.startswith('_'):
+    for col in df_append.columns.to_list():
+        if col.startswith('_'):
+            del df_append[col]
+    cond_satisfies_warning = len(df_append.columns)<2
+    if cond_satisfies_warning:
+        st.warning('Problem when column is only one. ValueError: setting an array element with a sequence')
+        df_append['__hidden']=df_append.index
+    df_append = st.data_editor(df_append,num_rows='dynamic',column_config=custom_configs_rw_append)
+    if cond_satisfies_warning:
+        st.warning('Problem when column is only one. ValueError: setting an array element with a sequence')
+        del df_append['__hidden']
+
+    "Conversion to ids"
+    for col in [col for col in df_append.columns.to_list() if col.endswith('__conversion')]:
+        original_col=col[:col.find('__conversion')]
+        df_append[original_col]=df_append[col].apply(lambda val:tss_foreign[original_col].get_local_val_to_id(selected_col_convert[original_col])[val])
         del df_append[col]
-cond_satisfies_warning = len(df_append.columns)<2
-if cond_satisfies_warning:
-    st.warning('Problem when column is only one. ValueError: setting an array element with a sequence')
-    df_append['__hidden']=df_append.index
-df_append = st.data_editor(df_append,num_rows='dynamic',column_config=custom_configs_rw_append)
-if cond_satisfies_warning:
-    st.warning('Problem when column is only one. ValueError: setting an array element with a sequence')
-    del df_append['__hidden']
 
-"Conversion to ids"
-for col in [col for col in df_append.columns.to_list() if col.endswith('__conversion')]:
-    original_col=col[:col.find('__conversion')]
-    df_append[original_col]=df_append[col].apply(lambda val:tss_foreign[original_col].get_local_val_to_id(selected_col_convert[original_col])[val])
-    del df_append[col]
-df_append
+    appends = df_append.to_dict(orient='records')
 
-appends = df_append.to_dict(orient='records')
-
-if st.button('append'):
-    second_ts.upload_appends(*appends)
-    st.toast('append')
-    st.rerun()
+    if st.form_submit_button('append'):
+        second_ts.upload_appends(*appends)
+        st.toast('append')
+        st.rerun()
--- table_editor/editor.py
+++ table_editor/editor.py
@@ -12,34 +12,36 @@ st.subheader('edit mode')
 with st.sidebar:
     df_with_tag = bp.CheckPointFunction(stglobal.iter_tag_process)(second_ts).filter_tag()
 
-temp=second_ts.get_foreign_tables()
-for col in temp:
-    bb=second_ts.check_selfref_table(temp[col])
+with st.form('edit form',clear_on_submit=True):
 
-custom_configs_rw_def:dict = bp.CheckPointFunction(stglobal.iter_custom_column_configs)(second_ts).edit()
-custom_configs_rw_edit=custom_configs_rw_def.copy()
-df_edited = st.data_editor(df_with_tag,disabled=second_ts.column_identity,column_config=custom_configs_rw_edit)
+    temp=second_ts.get_foreign_tables()
+    for col in temp:
+        bb=second_ts.check_selfref_table(temp[col])
 
-def get_comparison(df_new,df_old):
-    def func_melt(df:pd.DataFrame):
-        df_reset = df.copy().reset_index()
-        return df_reset.melt(id_vars='id',value_name='_sn_value')
-    df_new_melt=func_melt(df_new)
-    df_old_melt=func_melt(df_old)
-    df_compared = df_new_melt.compare(df_old_melt)
-    changed=df_compared.index.to_list()
-    df_temp = df_new_melt.loc[changed]
-    recs=dict()
-    for temp in df_temp.to_dict('records'):
-        if temp['id'] not in recs:
-            recs[temp['id']] = {}
-        recs[temp['id']][temp['variable']] = temp['_sn_value']
-    return recs
+    custom_configs_rw_def:dict = bp.CheckPointFunction(stglobal.iter_custom_column_configs)(second_ts).edit()
+    custom_configs_rw_edit=custom_configs_rw_def.copy()
+    df_edited = st.data_editor(df_with_tag,disabled=second_ts.column_identity,column_config=custom_configs_rw_edit)
 
-recs=get_comparison(df_edited,df_with_tag)
+    def get_comparison(df_new,df_old):
+        def func_melt(df:pd.DataFrame):
+            df_reset = df.copy().reset_index()
+            return df_reset.melt(id_vars='id',value_name='_sn_value')
+        df_new_melt=func_melt(df_new)
+        df_old_melt=func_melt(df_old)
+        df_compared = df_new_melt.compare(df_old_melt)
+        changed=df_compared.index.to_list()
+        df_temp = df_new_melt.loc[changed]
+        recs=dict()
+        for temp in df_temp.to_dict('records'):
+            if temp['id'] not in recs:
+                recs[temp['id']] = {}
+            recs[temp['id']][temp['variable']] = temp['_sn_value']
+        return recs
 
-if st.button('upload'):
-    for row_id in recs:
-        st.toast(f'{row_id}:{recs[row_id]}')
-        second_ts.upload(id_row=row_id,**recs[row_id])
-    st.rerun()
+    recs=get_comparison(df_edited,df_with_tag)
+
+    if st.form_submit_button('upload'):
+        for row_id in recs:
+            st.toast(f'{row_id}:{recs[row_id]}')
+            second_ts.upload(id_row=row_id,**recs[row_id])
+        st.rerun()
