326909ae4a3c1fce67f8c4bbfd67fb40ba50e0fe
Lua Jeon <luajeon506@gmail.com>
2024-11-13 15:04:21 +0900

    new: new function

--- table_editor/editor.py
+++ table_editor/editor.py
@@ -1,5 +1,5 @@
 import streamlit as st
-
+import pyplus.streamlit as stp
 import pyplus.sql as sqlp
 import pandas as pd
 import pyplus.builtin as bp
@@ -9,32 +9,35 @@ df_with_tag:pd.DataFrame = st.session_state['selected_table_dataframe']
 custom_configs_ro = st.session_state['selected_table_column_config_ro']
 custom_configs_rw_def = st.session_state['selected_table_column_config_rw_def']
 
-with st.form('edit form',clear_on_submit=True):
+tp = stp.TabsPlus(titles=['cell','replace'],layout='tab')
 
-    temp=second_ts.get_foreign_tables()
+with tp['cell']:
+    with st.form('edit form',clear_on_submit=True):
 
-    df_edited = st.data_editor(df_with_tag,disabled=second_ts.get_identity(),column_config=custom_configs_rw_def)
+        temp=second_ts.get_foreign_tables()
 
-    def get_comparison(df_new,df_old):
-        def func_melt(df:pd.DataFrame):
-            df_reset = df.copy().reset_index()
-            return df_reset.melt(id_vars='id',value_name='_sn_value')
-        df_new_melt=func_melt(df_new)
-        df_old_melt=func_melt(df_old)
-        df_compared = df_new_melt.compare(df_old_melt)
-        changed=df_compared.index.to_list()
-        df_temp = df_new_melt.loc[changed]
-        recs=dict()
-        for temp in df_temp.to_dict('records'):
-            if temp['id'] not in recs:
-                recs[temp['id']] = {}
-            recs[temp['id']][temp['variable']] = temp['_sn_value']
-        return recs
+        df_edited = st.data_editor(df_with_tag,disabled=second_ts.get_identity(),column_config=custom_configs_rw_def)
 
-    recs=get_comparison(df_edited,df_with_tag)
+        def get_comparison(df_new,df_old):
+            def func_melt(df:pd.DataFrame):
+                df_reset = df.copy().reset_index()
+                return df_reset.melt(id_vars='id',value_name='_sn_value')
+            df_new_melt=func_melt(df_new)
+            df_old_melt=func_melt(df_old)
+            df_compared = df_new_melt.compare(df_old_melt)
+            changed=df_compared.index.to_list()
+            df_temp = df_new_melt.loc[changed]
+            recs=dict()
+            for temp in df_temp.to_dict('records'):
+                if temp['id'] not in recs:
+                    recs[temp['id']] = {}
+                recs[temp['id']][temp['variable']] = temp['_sn_value']
+            return recs
 
-    if st.form_submit_button('upload'):
-        for row_id in recs:
-            st.toast(f'{row_id}:{recs[row_id]}')
-            second_ts.upload(id_row=row_id,**recs[row_id])
-        st.rerun()
+        recs=get_comparison(df_edited,df_with_tag)
+
+        if st.form_submit_button('upload'):
+            for row_id in recs:
+                st.toast(f'{row_id}:{recs[row_id]}')
+                second_ts.upload(id_row=row_id,**recs[row_id])
+            st.rerun()
