78954841ca72062c857a7cb33d363acd7ab8701b
Lua Jeon <luajeon506@gmail.com>
2024-12-22 23:42:29 +0900

    change: display

--- main.py
+++ main.py
@@ -110,7 +110,7 @@ def iter_tag_process(ts:sqlp.TableStructure,hashtag_init_symbol:str='#',hashtag_
                 
                 sr_tags_extracted=df[col_3].str.split(hashtag_init_symbol)\
                 .apply(extract_tags).apply(remove_spaces).apply(duplicate_super_tags)
-                selected_tags = st.multiselect(f'select tags of {f'_tags_{col_3}'}',find_all_tags(sr_tags_extracted),[])
+                selected_tags = st.multiselect(f'select tags of {col_3}',find_all_tags(sr_tags_extracted),[])
                 sr_selected_rows = sr_tags_extracted.apply(lambda ll:contains_tags(ll,selected_tags))
                 filt_rows[f'_tags_{col_3}'] = sr_selected_rows
 
