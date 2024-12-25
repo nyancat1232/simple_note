cf04381c674de2869b3416758a7f61596b22ccc0
Lua Jeon <luajeon506@gmail.com>
2024-12-25 22:53:05 +0900

    new: fragment

--- table_editor/remover_table.py
+++ table_editor/remover_table.py
@@ -7,24 +7,28 @@ df= st.session_state['selected_table_dataframe']
 df
 
 "select"
-event = st.dataframe(df,on_select='rerun',selection_mode=['multi-column','multi-row'])
-event
+@st.fragment
+def select_deletion():
+    event = st.dataframe(df,on_select='rerun',selection_mode=['multi-column','multi-row'])
+    event
 
-"remove col"
-df[event['selection']['columns']]
-event['selection']['columns']
-if st.button('delete columns'):
-    for col in event['selection']['columns']:
-        st.toast(col)
-        ts.delete_column(col)
-    st.rerun()
+    "remove col"
+    df[event['selection']['columns']]
+    event['selection']['columns']
+    if st.button('delete columns'):
+        for col in event['selection']['columns']:
+            st.toast(col)
+            ts.delete_column(col)
+        st.rerun()
 
-"remove row"
-df_filtered_row=df.iloc[event['selection']['rows']]
-rows=df_filtered_row.index.to_list()
-rows
-if st.button('delete rows'):
-    for row in rows:
-        st.toast(row)
-        ts.delete_row(row)
-    st.rerun()
+    "remove row"
+    df_filtered_row=df.iloc[event['selection']['rows']]
+    rows=df_filtered_row.index.to_list()
+    rows
+    if st.button('delete rows'):
+        for row in rows:
+            st.toast(row)
+            ts.delete_row(row)
+        st.rerun()
+
+select_deletion()
