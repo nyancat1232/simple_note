cefd3bc096a1fbdbcf2c77d554c6af72353234f1
Lua Jeon <luajeon506@gmail.com>
2025-01-01 02:24:51 +0900

    beautify:

--- table_editor/append_rows.py
+++ table_editor/append_rows.py
@@ -39,7 +39,10 @@ def append_rows(df_append:pd.DataFrame):
                                                                  key=f'convert_of_{col_local_foreign}'
                                                                  )['selection']['columns']
             if len(selected_col_convert[col_local_foreign])>0:
-                selections=dfs_foreign[col_local_foreign][selected_col_convert[col_local_foreign][0]].unique().dropna().tolist()
+                selections=(dfs_foreign[col_local_foreign][selected_col_convert[col_local_foreign][0]].unique()
+                                                                                                      .dropna()
+                                                                                                      .tolist()
+                )
                 custom_configs_rw_def[col_local_foreign+'__conversion']=st.column_config.SelectboxColumn(f'{col_local_foreign}(conversion from {selected_col_convert[col_local_foreign][0]})',
                                                                                                          options=selections)
                 df_append[col_local_foreign+'__conversion']=pd.Series()
